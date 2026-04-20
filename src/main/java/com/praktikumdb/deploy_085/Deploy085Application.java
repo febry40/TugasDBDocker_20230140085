f59641e6a9d4c53861669855ac86d5d218a0db7b
febry40 <febryevayonisa.com>
2026-04-20 13:06:30 +0700

    add Deploy

--- src/main/java/com/praktikumdb/deploy_085/Deploy085Application.java
+++ src/main/java/com/praktikumdb/deploy_085/Deploy085Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Deploy085Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Deploy085Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Deploy085Application.class, args);
+    }
 
 }
