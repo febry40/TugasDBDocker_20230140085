3ca84f63a0b38700ca680ce3078868b5d17258cb
febry40 <febryevayonisa.com>
2026-04-20 10:35:44 +0700

    menambahkan user entity model lombok&jpa

---
+++ src/test/java/com/praktikumdb/deploy_085/Deploy085ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikumdb.deploy_085;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy085ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
