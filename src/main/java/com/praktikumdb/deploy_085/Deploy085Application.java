0736dc3385cbd120144f6487984b53b4b1d09bb8
febry40 <febryevayonisa.com>
2026-04-20 10:41:27 +0700

    add initial project2

---
+++ src/main/java/com/praktikumdb/deploy_085/Deploy085Application.java
@@ -0,0 +1,13 @@
+package com.praktikumdb.deploy_085;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy085Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy085Application.class, args);
+	}
+
+}
